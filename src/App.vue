b24e8dd91f993647d7d86433e9c9047207d1ffb4
VallyZeng <735834365@qq.com>
2025-09-26 18:10:04 +0800

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <List />
 </template>
 
 <style scoped>
---
+++ src/components/List.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+
+const items = ([{message: 'Foo'}, {message: 'Bar' }])
+// 访问父属性
+const parentMessage = ref('Parent')
+
+const myObject = ({
+ title: 'How to do lists in Vue',
+ author: "Jone doe"
+})
+
+</script>
+
+<template>
+<li v-for="item in items">
+ {{ item.message }}
+</li>
+// 使用索引，访问父属性
+<li v-for="(item, index) in items">
+ {{ parentMessage }} - {{ index }} - {{ item }}
+</li>
+<p>遍历对象的所有属性</p>
+
+<ul>
+ <li v-for="value in myObject">
+  {{ value }}
+ </li>
+</ul>
+<p>多参数，第二个参数是属性名，第三个参数是索引</p>
+<ul>
+ <li v-for="(value, key, index) in myObject">
+  {{ value }} - {{ key }} - {{ index }}
+ </li>
+</ul>
+
+<p>在v-for中使用范围值</p>
+<span v-for="n in 3">{{ n }}</span>
+</template>
